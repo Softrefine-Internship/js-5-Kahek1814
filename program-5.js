9e0114ec961f91081e54a667a7fab953c8a64d2b
Kahek1814 <karan.softrefine@gmail.com>
2025-11-17 13:04:04 +0530

    Update program-5.js

--- program-5.js
+++ program-5.js
@@ -8,6 +8,7 @@ class Shape {
     return 0;
   }
 }
+
 class Circle extends Shape {
   constructor(radius) {
     super();
@@ -19,20 +20,20 @@ class Circle extends Shape {
   }
 }
 
-
 class Triangle extends Shape {
   constructor(base, height) {
     super();
     this.base = base;
     this.height = height;
   }
+
   calculateArea() {
     return 0.5 * this.base * this.height;
   }
 }
 
 const circle = new Circle(5);
-console.log(`Area of Circle: ${circle.calculateArea(2)}`);
+console.log(`Area of Circle: ${circle.calculateArea()}`);
 
 const triangle = new Triangle(10, 6);
-console.log(`Area of Triangle: ${triangle.calculateArea(2)}`);
+console.log(`Area of Triangle: ${triangle.calculateArea()}`);
